bb36cda8f59f822eb1e3fa01219d554c35c5355f
dips0042 <120435873+dips0042@users.noreply.github.com>
2026-06-23 21:32:21 +0700

    Create config.js

---
+++ views/config.js
@@ -0,0 +1,31 @@
+// MASTER DATA WILAYAH KOMSEL DAN KATEGORI BWC
+const BWC_MASTER_COOL = [
+    "Anointed Disciples",
+    "Compassion",
+    "Chosen",
+    "Favor of God",
+    "God's Will",
+    "Happiness",
+    "Living Sacrifice",
+    "Penuai",
+    "Precious",
+    "Radiance",
+    "Sahabat",
+    "Sonship",
+    "Teens",
+    "Prime",
+    "Belum Ada Komsel"
+];
+
+const BWC_MASTER_KATEGORI = [
+    { code: "FL", label: "FL (FrontLiners)" },
+    { code: "Leaders", label: "Leaders" },
+    { code: "B",  label: "B (Aktif BWC Only)" },
+    { code: "BC", label: "BC (Aktif BWC & Cool)" },
+    { code: "C",  label: "C (Aktif Cool Only)" },
+    { code: "I",  label: "I (Inaktif)" },
+    { code: "JB", label: "JB (Jiwa Baru)" },
+    { code: "BELUM COOL", label: "Belum Cool" }
+];
+
+const BASE_URL_API = "https://script.google.com/macros/s/AKfycbw7upfY0eEY-CIGGeH9o7gSmPCJNqx5dL1dlKDrOe4FmIHDlhIFqb8HeBQkMP4ycOsp/exec"
